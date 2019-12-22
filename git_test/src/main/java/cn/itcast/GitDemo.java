f18e07e0ef9477fdb08281ba6a49f2e27961a119
linzhaoyuan <403765726@qq.com>
2019-12-22 20:07:27 +0800

    11

--- git_test/src/main/java/cn/itcast/GitDemo.java
+++ git_test/src/main/java/cn/itcast/GitDemo.java
@@ -3,6 +3,6 @@ package cn.itcast;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("112");
-        System.out.println("123");
+        System.out.println("1231");
     }
 }
