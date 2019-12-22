dfab8b778b27578b70dd4c5226abef96bcce73ec
linzhaoyuan <59131002+linzhaoyuan@users.noreply.github.com>
2019-12-22 20:11:21 +0800

    Update GitDemo.java

--- git_test/src/main/java/cn/itcast/GitDemo.java
+++ git_test/src/main/java/cn/itcast/GitDemo.java
@@ -3,6 +3,7 @@ package cn.itcast;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("112");
-        System.out.println("1231");
+        System.out.println("123dsd1");
+
     }
 }
